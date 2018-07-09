404fa7b9c5b8466a7970f87307b8d0f79e5d98a0
Alsu <aaabdra5@gmail.com>
2018-07-09 14:56:31 +0300

    Отображение списка пользователей, Окно создания пользователя

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,20 @@
 import React, {Component} from 'react'
 import UserList from './UserList'
 import users from '../usersArr'
-import Modal from './CreateUser/index'
+import {CreateUser} from './CreateUser/index';
 
 class App extends Component {
-    constructor() {
-		super();
-
-		this.state = {
-			modal: false
-		};
-
-		this.renderModal = this.renderModal.bind(this);
-		this.modalToggle = this.modalToggle.bind(this);
-	}
-
-	modalToggle() {
-		this.setState({
-			modal: !this.state.modal
-        });
-	}
-
-	renderModal() {
-		return this.state.modal ? <Modal closeModal={this.modalToggle} /> : '';
-	}
-
     render() {
         return (
             <div className='container'>
-                    <h3 className='display-4' style={{padding: '40px'}}>
-                        Список пользователей
-                    </h3>
-                    <button className="btn btn-primary" onClick={this.modalToggle}>Создать пользователя</button>
-				    {this.renderModal()}
-                	<UserList users={users} />
-					<div class="btn-toolbar" role="toolbar" aria-label="...">
-						<div class="btn" aria-label="1">1</div>
-						<div class="btn" aria-label="2">2</div>
-						<div class="btn" aria-label="3">3</div>
-					</div>
+                <CreateUser/>
+                <h3 className style={{padding: '50px', font: '30px Open Sans Light', textTransform: 'uppercase'}}>
+                    Список пользователей
+                </h3>
+                <UserList users={users} />
             </div>
         )
     }
 }  
 
-export default App
+export default App;
--- src/components/UserCard/index.js
+++ src/components/UserCard/index.js
@@ -1,52 +1,22 @@
 import React, {Component} from 'react'
-import EditUser from './EditUser/index'
-
+import './style.css'
 
 class UserCard extends Component {
-
-    constructor() {
-		super()
-
-		this.state = {
-			modal: false
-		}
-
-		this.renderModal = this.renderModal.bind(this);
-		this.modalToggle = this.modalToggle.bind(this);
-    }
-    
-    modalToggle() {
-		this.setState({
-			modal: !this.state.modal
-        });
-	}
-
-	renderModal() {
-		return this.state.modal ? <EditUser closeModal={this.modalToggle} /> : '';
-	}
-
     render() {
         const {user} = this.props
         return (
             <div className="row">
-                <div className="col-md-12" style={{padding: '40px', paddingRight: '0px', borderBottom: '1px solid rgba(148, 159, 172, 0.4)'}}>
-                    <img className="float-left" src={user.image} width="80" height="80" alt="..."/>
-                    <div className="caption float-left" style={{paddingLeft: '30px'}}>
+                <div className="col">
+                    <img className="image" src={user.image} alt="..."/>
+                    <div className="caption">
                         <h3>
                             {user.name}
                         </h3>
-                        <p className='text-muted'>
+                        <p>
                             Дата регистрации: {(new Date(user.date)).toDateString()}
                         </p>
-                    </div>
-                    <div className="btn-group-vertical float-right">
-                        <button type="button" class="btn btn-primary" onClick={this.modalToggle}>
-                            Редактировать
-                        </button>
-                        {this.renderModal()}
-                        <button type="button" class="btn btn-default">
-                            Удалить
-                        </button>
+                        <button>Редактировать</button>
+                        <button>Удалить</button>
                     </div>
                 </div>
             </div>
